594133023341aa0b2a301331a4571b3cd74de693
Matheus Schiavão <matheus.schiavao08@gmail.com>
2022-04-26 18:27:53 -0300

    alteracao 2604

--- Menu.js
+++ Menu.js
@@ -30,3 +30,18 @@
    navOverlay.style.transition = "all 0.5s ease 0.3s";
    navOverlay.classList.remove("open");
  }
+
+ let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 3,5 seconds
+}
